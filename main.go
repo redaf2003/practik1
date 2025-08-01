4827b089f8857b6a00778b5ecd8d1641c24537c8
Artem danilov <zaxe2016@bk.ru>
2025-08-01 21:52:33 +1000

    Create several transactions and write them in order of processing. Process them one by one using the ProcessingTransactions function and in main.

--- main.go
+++ main.go
@@ -23,18 +23,53 @@ type Transaction struct {
 //	Users - всех зарегистрированных пользователей (ключ: ID, значение: данные пользователя)
 //	Transactions - полную историю всех платежей (список операций)
 type PaymentSystem struct {
-	Users        map[string]User // База пользователей (ID → данные)
-	Transactions []Transaction   // История всех платежных операций
+	Users        map[string]*User // База пользователей (ID → данные)
+	Transactions []Transaction    // История всех платежных операций
 }
 
 // Добавления пользователей в систему (берет ID пользователя и добавляет его в мапу).
-func (ps *PaymentSystem) AddUser(user User) {
-	ps.Users[user.Id] = user // Добавили нового пользователя в мапу
+func (ps *PaymentSystem) AddUser(u User) {
+	ps.Users[u.Id] = &u // Добавили нового пользователя в мапу
 }
 
 // Добавления транзакций в очередь (добавлие элемента в слайс)
-func (ps *PaymentSystem) AddTransaction(transaction Transaction) {
-	ps.Transactions = append(ps.Transactions, transaction) // Добавили транзакцию в слайс
+func (ps *PaymentSystem) AddTransaction(t Transaction) {
+	ps.Transactions = append(ps.Transactions, t) // Добавили транзакцию в слайс
+}
+
+func (ps *PaymentSystem) ProcessingTransactions() []error {
+	var errorsList []error
+
+	// Проходим по всем транзакциям
+	for _, transaction := range ps.Transactions {
+		fromUser, fromExists := ps.Users[transaction.FromID]
+		toUser, toExists := ps.Users[transaction.ToID]
+
+		// Проверка на существование отправителя
+		if !fromExists {
+			errorsList = append(errorsList, errors.New("пользователь с ID "+transaction.FromID+" не найден"))
+			continue
+		}
+
+		// Проверка на существование получателя
+		if !toExists {
+			errorsList = append(errorsList, errors.New("пользователь с ID "+transaction.ToID+" не найден"))
+			continue
+		}
+
+		// Проверка на успешность снятия средств
+		if err := fromUser.Withdraw(transaction.Amount); err != nil {
+			errorsList = append(errorsList, err)
+			continue
+		}
+
+		// Пополнение счета получателя
+		toUser.Deposit(transaction.Amount)
+	}
+
+	// Очищаем список транзакций после обработки
+	ps.Transactions = nil
+	return errorsList
 }
 
 // Структура User: ID (строка), Name (строка), Balance (число).
@@ -51,50 +86,47 @@ func (u *User) Deposit(amount float64) {
 
 // Реализуйте метод Withdraw для структуры User, который уменьшает баланс на указанную сумму.
 // Проверяйте достаточность средств, возвращая ошибку при нехватке.
-func (u *User) Withdraw(amounts float64) error {
-	if amounts <= 0 {
-		return errors.New("Cумма должна быть положительной")
+func (u *User) Withdraw(amount float64) error {
+	if u.Balance < amount {
+		return errors.New("Баланс меньше чем сумма перевода")
 	}
-	if amounts > u.Balance {
-		return errors.New("недостаточно средств")
-	}
-	u.Balance -= amounts
-
+	u.Balance -= amount
 	return nil
 }
 
 func main() {
-	//Создайте несколько объектов пользователей с разными значениями баланса и именами.
-	user1 := &User{
-		Id:      "1",
-		Name:    "Artem",
-		Balance: 500.0,
+	// Создаем экземпляр PaymentSystem
+	paymentSystem := PaymentSystem{
+		Users:        make(map[string]*User),
+		Transactions: []Transaction{},
 	}
 
-	user2 := &User{
-		Id:      "2",
-		Name:    "Egor",
-		Balance: 300.0,
+	// Создаем пользователей
+	user1 := User{Id: "1", Name: "Тёмочка", Balance: 100.0}
+	user2 := User{Id: "2", Name: "Бро", Balance: 50.0}
+	user3 := User{Id: "3", Name: "Маша", Balance: 200.0}
+
+	// Добавляем пользователей в систему
+	paymentSystem.AddUser(user1)
+	paymentSystem.AddUser(user2)
+	paymentSystem.AddUser(user3)
+
+	// Добавляем несколько транзакций
+	paymentSystem.AddTransaction(Transaction{FromID: "1", ToID: "2", Amount: 30})
+	paymentSystem.AddTransaction(Transaction{FromID: "2", ToID: "3", Amount: 20})
+	paymentSystem.AddTransaction(Transaction{FromID: "3", ToID: "1", Amount: 50})
+
+	// Обрабатываем транзакции
+	if errList := paymentSystem.ProcessingTransactions(); len(errList) > 0 {
+		for _, err := range errList {
+			fmt.Println("Ошибка:", err)
+		}
+	} else {
+		fmt.Println("Транзакции успешно обработаны!")
 	}
 
-	//Провели операции пополнения и снятия средств. После каждой операции выводите баланс.
-	fmt.Println("\nНачальные балансы:")
-	fmt.Printf("User1: %+v\n", user1)
-	fmt.Printf("User2: %+v\n", user2)
-
-	fmt.Println("\nПосле пополнения:")
-	user1.Deposit(200)
-	user2.Deposit(300)
-	fmt.Printf("User1: %+v\n", user1)
-	fmt.Printf("User2: %+v\n", user2)
-
-	fmt.Println("\nПосле снятия:")
-	if err := user1.Withdraw(1000); err != nil {
-		fmt.Println("Ошибка:", err)
-	}
-	if err := user2.Withdraw(200); err != nil {
-		fmt.Println("Ошибка:", err)
-	}
-	fmt.Printf("User1: %+v\n", user1)
-	fmt.Printf("User2: %+v\n", user2)
+	// Проверяем балансы
+	fmt.Println("Баланс Тёмочки:", paymentSystem.Users["1"].Balance)
+	fmt.Println("Баланс Бро:", paymentSystem.Users["2"].Balance)
+	fmt.Println("Баланс Маши:", paymentSystem.Users["3"].Balance)
 }
